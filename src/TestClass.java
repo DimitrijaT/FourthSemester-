6683d00c1f5fa188afbe4f678d55eb35c23bbda9
Dimitrija Timeski <dimitrijatimeskidimitrija@gmail.com>
2022-03-13 19:09:56 +0100

    Parrot + forgot loop

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,5 +1,10 @@
 public class TestClass {
     public static void main(String[] args) {
         System.out.println("Zdravo!");
+        System.out.println("Parrot!");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
     }
 }
